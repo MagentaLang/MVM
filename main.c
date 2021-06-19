f23353bd3101ca2fc5332dfc7a88f4bf3d0a35d4
alice <notminin@pm.me>
2021-06-19 21:56:28 +0100

    'implement' setting byte width flag

--- main.c
+++ main.c
@@ -10,7 +10,7 @@ void usage() {
 
 enum mode {
 	normal,
-	opush,
+	push,
 };
 
 int main(int argc, char *argv[]) {
@@ -28,12 +28,12 @@ int main(int argc, char *argv[]) {
 		int size = ftell(f);
 		fseek(f, 0, SEEK_SET);
 
-		// create stacks
-		char bytecode[size]; // bytecode
-		char mstack[1024]; // memory
+		char mstack[256]; // memory
+		int width = 1; // byte width
 		stack *sstack = initstack(1024); // stack
 		// create program stack
 		int counter = 0;
+		char bytecode[size]; // bytecode
 		size_t ret_code = fread(bytecode, sizeof(char), size, f);
 		if (ret_code == size) {
 			// good :)
@@ -47,7 +47,7 @@ int main(int argc, char *argv[]) {
 			}
 		}
 
-		// print program stack
+		// execute program
 		enum mode mode = normal;
 		while (counter < size) {
 			char c = bytecode[counter];
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]) {
 			switch (mode) {
 				case normal: switch (c) {
 					case 0x00: {
-						mode = opush;
+						mode = push;
 					} break;
 
 					case 0x02: {
@@ -63,17 +63,7 @@ int main(int argc, char *argv[]) {
 							putchar(stack_pop(sstack));
 					} break;
 
-					case 0x08: case 0x09: case 0x0b: case 0x0c: {
-						char a = stack_pop(sstack);
-						char b = stack_pop(sstack);
-
-						if (c == 0x08) stack_push(sstack, a + b);
-						if (c == 0x09) stack_push(sstack, a - b);
-						if (c == 0x0b) stack_push(sstack, a * b);
-						if (c == 0x0c) stack_push(sstack, a / b);
-					} break;
-
-					case 0x13: {
+					case 0x04: {
 						char c = stack_pop(sstack);
 						char str[4];
 
@@ -84,6 +74,19 @@ int main(int argc, char *argv[]) {
 							stack_push(sstack, str[i]);
 					} break;
 
+					case 0x0A: case 0x0B: {
+						char a = stack_pop(sstack);
+						char b = stack_pop(sstack);
+
+						if (c == 0x08) stack_push(sstack, a + b);
+						if (c == 0x09) stack_push(sstack, a - b);
+					} break;
+
+					case 0x12: width = 1; break;
+					case 0x13: width = 2; break;
+					case 0x14: width = 4; break;
+					case 0x15: width = 8; break;
+
 					default: {
 						err("fatal: invalid opcode");
 						printf("\n          [%d] 0x%x <--\n", counter + 1, c);
@@ -91,13 +94,15 @@ int main(int argc, char *argv[]) {
 					}
 				} break;
 
-				case opush:
+				case push:
 					switch (c) {
 					case 0x00: {
+						// TODO: check for consecutive null bytes the length of
+						// memory width
 						mode = normal;
 					} break;
 
-					case 0x1A: {
+					case 0xFF: {
 						stack_push(sstack, 0);
 					} break;
 
